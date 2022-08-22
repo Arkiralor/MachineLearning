3b56a70008172d033faf636b70787b1c549a8439
Arkiralor <prithoo11335@gmail.com>
2022-08-22 08:45:30 +0530

    new model flow

--- src/data_frame.py
+++ src/data_frame.py
@@ -11,9 +11,12 @@ dataset = path.join(BASE_DIR, "data", "vgsales_cleaned.csv")
 DATA_FRAME = pd.read_csv(dataset)
 
 ## preliminary cleaning
-DATA_FRAME = DATA_FRAME.drop(columns=['Rank', 'Name'], axis=1)
+DATA_FRAME = DATA_FRAME.drop(columns=['Unnamed: 0', 'Rank', 'Name'], axis=1)
 DATA_FRAME = DATA_FRAME.dropna()
 
 GENRE = {i: j for i, j in enumerate(DATA_FRAME['Genre'].unique().tolist())}
 PLATFORM = {i: j for i, j in enumerate(DATA_FRAME['Platform'].unique().tolist())}
 PUBLISHER = {i: j for i, j in enumerate(DATA_FRAME['Publisher'].unique().tolist())}
+
+if __name__=="__main__":
+    print(DATA_FRAME.head(16))
